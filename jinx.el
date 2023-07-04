16eab0ec2eee159512436ebad852c710917f77a1
Daanturo <daanturo@gmail.com>
2023-07-04 18:44:47 +0700

    Add separate jinx-correct-visible and jinx-correct-buffer
    extracted from jinx-correct.
    
    Rename jinx-correct-word-at-point to jinx-correct-at-point

--- jinx.el
+++ jinx.el
@@ -743,6 +743,27 @@ If CHECK is non-nil, always check first."
   (modify-syntax-entry ?’ "w" jinx--syntax-table)
   (modify-syntax-entry ?. "." jinx--syntax-table))
 
+(defun jinx--correct-overlays (overlays &optional show-count)
+  "Correct words at OVERLAYS.
+If SHOW-COUNT is non-nil, show the index of the correcting words."
+  (cl-letf* (((symbol-function #'jinx--timer-handler) #'ignore) ;; Inhibit
+             (repeat-mode nil) ;; No repeating of jinx-next and jinx-previous
+             (count (length overlays))
+             (idx 0))
+    (unwind-protect
+        (while (when-let ((ov (nth idx overlays)))
+                 (let* ((deleted (not (overlay-buffer ov)))
+                        (skip
+                         (catch 'jinx--goto
+                           (unless deleted
+                             (jinx--correct
+                              (overlay-start ov)
+                              (overlay-end ov)
+                              (and show-count (format " (%d of %d)" (1+ idx) count)))))))
+                   (cond
+                    ((integerp skip) (setq idx (mod (+ idx skip) count)))
+                    ((or show-count deleted) (cl-incf idx)))))))))
+
 ;;;; Save functions
 
 (defun jinx--save-action (key word ann)
@@ -826,6 +847,30 @@ With prefix argument GLOBAL change the languages globally."
   (jinx--cleanup))
 
 ;;;###autoload
+(defun jinx-correct-visible ()
+  "Correct visibly misspelled words in current window."
+  (interactive)
+  (unless jinx-mode (jinx-mode 1))
+  (let* ((old-point (point-marker)))
+    (unwind-protect
+        (jinx--correct-overlays
+         (jinx--force-overlays (window-start) (window-end) :visible t))
+      (goto-char old-point)
+      (jinx--in-base-buffer #'jit-lock-refontify (window-start) (window-end)))))
+
+;;;###autoload
+(defun jinx-correct-buffer ()
+  "Correct all misspelled words in current buffer."
+  (interactive)
+  (unless jinx-mode (jinx-mode 1))
+  (push-mark)
+  (unwind-protect
+      (jinx--correct-overlays
+       (jinx--force-overlays (point-min) (point-max) :check t)
+       t)
+    (jinx--in-base-buffer #'jit-lock-refontify)))
+
+;;;###autoload
 (defun jinx-correct (&optional all)
   "Correct nearest misspelled word.
 If prefix argument ALL non-nil correct all misspellings."
@@ -861,7 +906,7 @@ If prefix argument ALL non-nil correct all misspellings."
         (jinx--in-base-buffer #'jit-lock-refontify (window-start) (window-end))))))
 
 ;;;###autoload
-(defun jinx-correct-word-at-point ()
+(defun jinx-correct-at-point ()
   "Correct word at cursor.
 Suggest corrections even if it's not misspelled."
   (interactive)
